a5be9d345bdae58343a3beb5ad83725c4de8fe20
Suraj Manik More <surajmore7858@gmail.com>
2026-07-21 13:52:54 +0530

    ready for deployment

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const rateLimit = require('express-rate-limit');
 app.set('views', patho.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 var corsOptions = {
-    origin: "http://localhost:8081"
+    origin: process.env.CLIENT_URL || "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
